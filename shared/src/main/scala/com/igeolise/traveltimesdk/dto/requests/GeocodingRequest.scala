1ae490d71f0f6b078c62a895a2846bd1ed03cee9
lukasB <lukas.bloznelis@gmail.com>
2019-05-27 17:11:09 +0300

    Update after PR review -Moved `ReverseGeocodingRequest` out of timefilter package -Added Scaladoc for Geocoding and ReverseGeocoding requests.

--- shared/src/main/scala/com/igeolise/traveltimesdk/dto/requests/GeocodingRequest.scala
+++ shared/src/main/scala/com/igeolise/traveltimesdk/dto/requests/GeocodingRequest.scala
@@ -8,6 +8,18 @@ import com.igeolise.traveltimesdk.dto.responses.{GeoJsonResponse, TravelTimeSdkE
 import com.igeolise.traveltimesdk.json.reads.GeocodingReads._
 import com.softwaremill.sttp.{Uri, _}
 
+/**
+  * Match a query string to geographic coordinates.
+  *
+  * @param query       A query to geocode. Can be an address, a postcode or a venue. For example SW1A 0AA or
+  *                    Victoria street, London. Providing a country or city the request will get you more
+  *                    accurate results.
+  * @param focusCoords Latitude and longitude of a focus point. This will prioritize results around this point.
+  *                    Note that this does not exclude results that are far away from the focus point.
+  * @param countryCode Country code in ISO 3166-1 alpha-2 or alpha-3
+  *                    [[https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes Country codes]].
+  *                    Only return the results that are within the specified country.
+  */
 case class GeocodingRequest(
   query: String, focusCoords: Option[Coords] = None, countryCode: Option[String] = None
 ) extends TravelTimePlatformRequest[GeoJsonResponse[GeocodingResponseProperties]]  {
--- shared/src/main/scala/com/igeolise/traveltimesdk/dto/requests/ReverseGeocodingRequest.scala
+++ shared/src/main/scala/com/igeolise/traveltimesdk/dto/requests/ReverseGeocodingRequest.scala
@@ -1,15 +1,21 @@
-package com.igeolise.traveltimesdk.dto.requests.timefilter
+package com.igeolise.traveltimesdk.dto.requests
 
 import cats.Monad
 import com.igeolise.traveltimesdk.dto.common.Coords
-import com.igeolise.traveltimesdk.dto.requests.RequestUtils
 import com.igeolise.traveltimesdk.dto.requests.RequestUtils.TravelTimePlatformRequest
-import com.igeolise.traveltimesdk.dto.responses.{GeoJsonResponse, TravelTimeSdkError}
 import com.igeolise.traveltimesdk.dto.responses.common.GeocodingResponseProperties
+import com.igeolise.traveltimesdk.dto.responses.{GeoJsonResponse, TravelTimeSdkError}
 import com.softwaremill.sttp.{sttp, Request, Uri}
 import com.igeolise.traveltimesdk.json.reads.GeocodingReads._
-import scala.language.higherKinds
 
+/**
+  * Attempt to match a latitude, longitude pair to an address.
+  *
+  * @param coordinates Coordinates to match.
+  * @param withinCountry Country code in ISO 3166-1 alpha-2 or alpha-3
+  *                      [[https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes Country codes]].
+  *                      Useful when performing reverse geocoding requests near a border.
+  */
 case class ReverseGeocodingRequest(
   coordinates: Coords,
   withinCountry: Option[String]
