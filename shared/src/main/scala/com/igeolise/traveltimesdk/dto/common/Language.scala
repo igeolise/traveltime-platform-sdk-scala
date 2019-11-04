caa8bb056edcc4139efcd547fd1cb5a9ff524223
Zygimantas <zygimantas@benetis.me>
2019-11-04 16:03:49 +0200

    feat(geocoding) remove unused standard

--- shared/src/main/scala/com/igeolise/traveltimesdk/dto/common/Language.scala
+++ shared/src/main/scala/com/igeolise/traveltimesdk/dto/common/Language.scala
@@ -2,6 +2,3 @@ package com.igeolise.traveltimesdk.dto.common
 
 sealed trait LanguageTag
 case class BCP47(value: String) extends LanguageTag
-case class ISO6391(value: String) extends LanguageTag {
-  def toBCP47: BCP47 = BCP47(value)
-}
